cf320226a00e632a926d32b7d590971dc7699365
Joris Kersten <49530289+joriskersten@users.noreply.github.com>
2024-07-05 12:52:19 +0200

    - The MANUALCLEAN3GC/slurm_wsclean_automask_ALL.sh program now has default values that make sense for a 3GC run. - The createdirs.sh script now creates 'Briggs' directories instead of masked and maskless temporary directories.

--- MANUALCLEAN3GC/createdirs.sh
+++ MANUALCLEAN3GC/createdirs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir IMAGES
-mkdir masked_temp
-mkdir maskless_temp
+# mkdir masked_temp
+# mkdir maskless_temp
 mkdir automask_temp
+mkdir BriggsMinusZeroPointZeroFive
+mkdir BriggsMinusZeroPointThree
